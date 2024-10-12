3470f5ed15ecdc1c230ccaf7e7b99efa376ec4c8
VARUNRAMADURGAM <158152153+VARUNRAMADURGAM@users.noreply.github.com>
2024-10-12 18:06:54 +0530

    code for jwt authentication

---
+++ JWTDemo/src/main/java/com/example/JWTDemo/controller/AuthenticationController.java
@@ -0,0 +1,53 @@
+package com.example.JWTDemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.JWTDemo.DTO.AuthenticationRequest;
+import com.example.JWTDemo.DTO.AuthenticationResponse;
+import com.example.JWTDemo.security.MyUserDetailsService;
+import com.example.JWTDemo.util.JwtUtil;
+
+@RestController
+@RequestMapping("/jwt")
+public class AuthenticationController {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private JwtUtil jwtTokenUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@PostMapping("/authenticate")
+	public AuthenticationResponse createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (AuthenticationException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtTokenUtil.generateToken(userDetails.getUsername());
+
+		return new AuthenticationResponse(jwt);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
+}
